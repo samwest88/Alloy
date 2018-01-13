c4f7d04b3dcdefdd29e3d33ef970201f60a5fa8a
Sam West <sam.west@hotmail.co.uk>
2018-01-13 16:42:25 +0000

    Updates after reinstalling alloy

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Alloy")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d3b9ab24-dc32-451c-aaa3-ee6c1dff0990")]
+[assembly: Guid("1290e12f-5ce7-4b39-b123-fcbb28db9939")]
 
 // Version information for an assembly consists of the following four values:
 //
